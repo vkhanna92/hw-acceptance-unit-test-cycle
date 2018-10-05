d0970a87c2d61a830a96a2ba57bbe2aa5b4c398b
EC2 Default User <ec2-user@ip-172-31-40-205.us-east-2.compute.internal>
2018-10-05 03:42:33 +0000

    Last test case

--- rottenpotatoes/spec/models/model_spec.rb
+++ rottenpotatoes/spec/models/model_spec.rb
@@ -2,19 +2,18 @@ require 'rails_helper'
 
 describe Movie do
   describe '.find_similar_movies' do
-    let!(:movie1) { FactoryGirl.create(:movie, title: 'Jurassic Park', director: 'Steven Spielberg') }
+    let!(:movie1) { FactoryGirl.create(:movie, title: 'Catch me if you can', director: 'Steven Spielberg') }
     let!(:movie2) { FactoryGirl.create(:movie, title: 'Seven', director: 'David Fincher') }
-    let!(:movie3) { FactoryGirl.create(:movie, title: "Harry Potter", director: 'David Yates') }
-    let!(:movie4) { FactoryGirl.create(:movie, title: "Horrible Bosses") }
+    let!(:movie3) { FactoryGirl.create(:movie, title: "Schindler's List", director: 'Steven Spielberg') }
+    let!(:movie4) { FactoryGirl.create(:movie, title: "Stop") }
 
     context 'director exists' do
       it 'finds similar movies correctly' do
-        expect(Movie.similar_movies(movie1.title)).to eql(['Harry Potter', "David Yates"])
+        expect(Movie.similar_movies(movie1.title)).to eql(['Catch me if you can', "Schindler's List"])
         expect(Movie.similar_movies(movie1.title)).to_not include(['Seven'])
         expect(Movie.similar_movies(movie2.title)).to eql(['Seven'])
       end
     end
-
   end
 
   describe '.all_ratings' do
